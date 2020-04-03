d40a272763e5daf61e2edfd4e43420ab30d97464
Ian Griffiths <Ian.Griffiths@endjin.com>
2020-04-03 08:11:53 +0100

    Create initial project and workflow

---
+++ src/Idg.CSharpNuGetActionsExemplar/Information.cs
@@ -0,0 +1,7 @@
+﻿namespace Idg.CSharpNuGetActionsExemplar
+{
+    public static class Information
+    {
+        public static string Description => "This library is part of an exemplar project showing how to set up a GitHub repo that builds a NuGet package with CI/CD";
+    }
+}
